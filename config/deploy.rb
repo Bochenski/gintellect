66c6674fee29eb666b21a576bf0981704a18641e
David Bochenski <david@bochenski.co.uk>
2011-02-25 11:42:26 +0000

    updating deploy script to ruby 1.9.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :stages, %w(edge production)
 set :default_stage, "production"
 
 require '~/.rvm/lib/rvm/capistrano' # Adjust for load path
-set :rvm_ruby_string, 'ruby-1.8.7-p299' # Or whatever env you want it to run in.
+set :rvm_ruby_string, 'ruby-1.9.2-p136' # Or whatever env you want it to run in.
 set :rvm_type, :user
 
 
